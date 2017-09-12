02d75edddc3baeb6020de86e2261332de82a5152
Rituraj <psycic03@gmail.com>
2017-09-12 23:28:33 +0530

    Update QuickSort

--- DS In Java/QuickSort.java
+++ DS In Java/QuickSort.java
@@ -1,4 +1,3 @@
-package javaapplication2;
 
 public class QuickSort {
     private int pIndex;
@@ -6,12 +5,12 @@ public class QuickSort {
         int pivot=a[right];
         pIndex=left;
         for(int i=left;i<right;i++){
-        if(a[i]<pivot){
-            int temp=a[pIndex];
-            a[pIndex]=a[i];
-            a[i]=temp;
-             pIndex+=1;
-        }
+			if(a[i]<pivot){
+				int temp=a[pIndex];
+				a[pIndex]=a[i];
+				a[i]=temp;
+				pIndex+=1;
+			}
         }
         int temp=a[pIndex];
         a[pIndex]=a[right];
@@ -20,22 +19,21 @@ public class QuickSort {
     }
     public void QuickSort(int[] a,int left,int right){
         if(right>left){
-    pIndex=partition(a,left,right);
-    QuickSort(a,left,pIndex-1);
-    QuickSort(a,pIndex+1,right);
-    }
+			pIndex=partition(a,left,right);
+			QuickSort(a,left,pIndex-1);
+			QuickSort(a,pIndex+1,right);
+		}
     }
     public void Print(int[] a){
         for(int i=0;i<a.length;i++){
-        System.out.print(a[i]+" ");
+				System.out.print(a[i]+" ");
         }
     }
       
     public static void main(String[] args){
         QuickSort q=new QuickSort();
-        int[] a={2,5,1,43,7,21,88};
+        int[] a={55,44,33,22,11,9,8};
         q.QuickSort(a,0,a.length-1);
         q.Print(a);
     }
-    
 }
